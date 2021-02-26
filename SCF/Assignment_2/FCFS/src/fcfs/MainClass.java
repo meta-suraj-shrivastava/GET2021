7bd8032035eac719b748e932233ac7388517e955
Suraj Shrivastava <suraj.shrivastava@metacube.com>
2021-02-26 23:32:43 +0530

    Assignment 2 solution 2

---
+++ SCF/Assignment_2/FCFS/src/fcfs/MainClass.java
@@ -0,0 +1,95 @@
+package fcfs;
+
+import java.util.Scanner;
+public class MainClass {
+
+	static int[][] processData = new int[10][10];
+	static int[] completionTime = new int[10];
+	static int[] turnaroundTime = new int[10];
+	static int[] waitingTime = new int[10];
+	static int processes;
+static void sortAccArivalTime(){
+	for(int i=0;i<processes;i++){
+		for(int j=1;j<processes-i;j++){
+			if(processData[j][0]<processData[j-1][0]){
+				int temp = processData[j-1][0];
+				processData[j-1][0] = processData[j][0];
+				processData[j][0] = temp;
+				temp = processData[j-1][1];
+				processData[j-1][1] = processData[j][1];
+				processData[j][1] = temp;
+			}
+		}
+	}
+}
+
+
+static void printArray(int[] arr){
+	for(int i=0;i<processes;i++ )
+		System.out.print(arr[i]+" ");
+	System.out.println();
+}
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number of Processes Max 10 ");
+		processes = sc.nextInt();
+		System.out.println("Enter arrival Time and Burst Time respectively\n ");
+		for(int i=0;i<processes;i++){
+			for(int j=0;j<2;j++){
+				processData[i][j] = sc.nextInt();
+				
+			}
+		}
+		sortAccArivalTime();
+		calculateCompletionTime();
+		System.out.print("Completion Time : ");
+		printArray(completionTime);
+		System.out.print("Turnaround Time : ");
+		calculateTurnAroundTime();
+		printArray(turnaroundTime);
+		calculateWaitingTime();
+		System.out.print("Waiting Time : ");
+		printArray(waitingTime);
+		System.out.println("Avg waiting  Time : "+avgWaitingTime());
+		System.out.println("Max. waiting  Time : "+maxWaitingTime());
+		
+	}
+	
+	static void calculateCompletionTime(){
+		completionTime[0]=processData[0][1];
+		for(int i=1;i<processes;i++){
+			if(processData[i][0]<=completionTime[i-1])
+				completionTime[i]=completionTime[i-1]+processData[i][1];
+			else
+				completionTime[i]=processData[i][0]+processData[i][1];
+		}
+		
+	}
+	static void calculateTurnAroundTime(){
+		for(int i=0;i<processes;i++){
+			turnaroundTime[i]=completionTime[i]-processData[i][0];
+		}
+		
+	}
+	static void calculateWaitingTime(){
+		for(int i=0;i<processes;i++){
+			waitingTime[i]=turnaroundTime[i]-processData[i][1];
+		}	
+	}
+	static int maxWaitingTime(){
+		int max = waitingTime[0];
+		for(int i=1;i<processes;i++){
+			if(max<waitingTime[i])
+				max=waitingTime[i];
+		}
+		return max;
+	}
+	static float avgWaitingTime(){
+		int totalTime = 0;
+		for(int i=0;i<processes;i++){
+				totalTime+=waitingTime[i];
+		}
+		return totalTime/processes;
+	}
+}
