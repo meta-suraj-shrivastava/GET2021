df9963f36d51c5dca7387de3cb7d64a7870b1a31
Suraj Shrivastava <suraj.shrivastava@metacube.com>
2021-02-26 12:17:26 +0530

    Separate Assignments

---
+++ HexCalc/src/hexCalc/MainClass.java
@@ -0,0 +1,10 @@
+package hexCalc;
+
+public class MainClass {
+
+	public static void main(String[] args){
+		HexCalc hexCalc = new HexCalc();
+		System.out.println(hexCalc.add("A12F", "FF12"));
+	}
+	
+}
